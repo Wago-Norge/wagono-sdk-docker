15c2c8c43cc434e10b7b5b937539dcf14753bf69
Thomas Sandberg <thomas@sandberg.cloud>
2023-02-10 09:01:32 +0100

    added submodule update command

--- resources/workspaces/scripts/sdk_build.sh
+++ resources/workspaces/scripts/sdk_build.sh
@@ -5,7 +5,8 @@ elif [ $1 = "cc100" ]
 then
     echo "CC100 selected"
     echo "**************"
-    cd /workspaces/wagono-ptxdist-config/sdk-cc100/
+    cd /workspaces/src/sdk-cc100/
+    git submodule update --init
     ptxdist select configs/cc100/ptxconfig_generic
     ptxdist platform configs/cc100/platformconfig
     ptxdist toolchain /opt/gcc-Toolchain-2019.12/arm-linux-gnueabihf/bin
