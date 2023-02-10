59057173fe659b22a096adeffe74517c893a3be4
Thomas Sandberg <thomas@sandberg.cloud>
2023-02-10 10:13:49 +0100

    Update sdk_build.sh

--- resources/workspaces/scripts/sdk_build.sh
+++ resources/workspaces/scripts/sdk_build.sh
@@ -1,22 +1,6 @@
-if [ -z "$1" ]
-then
-    echo "Platform not specified! Choices: cc100, pfc or tp"
-elif [ $1 = "cc100" ]
-then
-    echo "CC100 selected"
-    echo "**************"
-    cd /workspaces/src/sdk-cc100/
-    git submodule update --init
-    ptxdist select configs/cc100/ptxconfig_generic
-    ptxdist platform configs/cc100/platformconfig
-    ptxdist toolchain /opt/gcc-Toolchain-2019.12/arm-linux-gnueabihf/bin
-    ptxdist go -q
-elif [ $1 = "pfc" ]
-then
-    echo "PFC selected"
-elif [ $1 = "tp" ]
-then
-    echo "TP/Edge selected"
-else
-    echo "Unknown platform! Choices: cc100, pfc or tp"
-fi
+cd /workspaces/src/sdk-cc100/
+git submodule update --init
+ptxdist select configs/cc100/ptxconfig_generic
+ptxdist platform configs/cc100/platformconfig
+ptxdist toolchain /opt/gcc-Toolchain-2019.12/arm-linux-gnueabihf/bin
+ptxdist go -q
