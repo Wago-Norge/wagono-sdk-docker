8af5857b22982327f51f9d1ed3bf0c68f86523c4
Thomas Sandberg <thomas@sandberg.cloud>
2023-02-10 12:03:03 +0100

    Delete sdk_build.sh

--- resources/workspaces/scripts/sdk_build.sh
+++
@@ -1,6 +0,0 @@
-cd /workspaces/src/sdk-cc100/
-git submodule update --init
-ptxdist select configs/cc100/ptxconfig_generic
-ptxdist platform configs/cc100/platformconfig
-ptxdist toolchain /opt/gcc-Toolchain-2019.12/arm-linux-gnueabihf/bin
-ptxdist go -q
